a27e202ee71b85a8bc4fdfbbd7096ce72efb9dc7
yashagrawal39 <yash21296@gmail.com>
2021-02-18 15:12:52 +0530

    responsive

--- js/script.js
+++ js/script.js
@@ -126,6 +126,12 @@ $('.customerslider').slick({
 	{
 		breakpoint: 991,
 		settings: {
+		slidesToShow: 3,
+		slidesToScroll: 1
+		}
+	},{
+		breakpoint: 767,
+		settings: {
 		slidesToShow: 2,
 		slidesToScroll: 1
 		}
